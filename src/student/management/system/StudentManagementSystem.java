ff7049c6e807803c011fc16969d1b74a8c81be70
buhle-cyber <buhlepinana4@gmail.com>
2026-07-29 18:48:35 +0200

    Initial commit: Student Management System

---
+++ src/student/management/system/StudentManagementSystem.java
@@ -0,0 +1,14 @@
+
+package student.management.system;
+
+/**
+ *
+ * @author buhle
+ */
+public class StudentManagementSystem {
+    public static void main(String[] args) {
+        StudentForm form = new StudentForm();
+        form.setVisible(true);
+    }
+    
+}
